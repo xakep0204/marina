185f39b331dc9c6f030511ee0f82e2e76e5ae0b2
xakep0204 <abhimanyu@attrish.in>
2023-07-23 19:59:24 +0530

    IMU first library

---
+++ IMU_Setup/IMU_Setup.ino
@@ -0,0 +1,44 @@
+#include <basicMPU6050.h>
+
+
+// Constants
+
+#define RAD_TO_DEG 180.00
+
+// IMU object setup
+
+basicMPU6050<> imu;
+
+
+float bAx;
+
+
+
+void setup() {
+  // setting up registers
+  imu.setup();
+
+  imu.setBias();
+  
+  // Serial Monitor
+
+  Serial.begin(38400);
+
+
+
+}
+
+void loop() {
+
+  imu.updateBias();
+
+  float Ax = imu.ax();
+
+  Serial.print( Ax );
+  Serial.print( " " );
+  Serial.print( imu.ay() );
+  Serial.print( " " );
+  Serial.print( imu.az() );
+  Serial.print( "    " );
+
+}
